b596c5d9d1d816c8931ea01181c35585aeb5bc5d
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2025-10-30 16:06:25 +0300

    ref

--- src/test/java/model/ProductsList.java
+++ src/test/java/model/ProductsList.java
@@ -6,8 +6,10 @@ public class ProductsList {
     private List<Product> products;
     
     public String getShopName() { return shopName; }
+
     public void setShopName(String shopName) { this.shopName = shopName; }
     
     public List<Product> getProducts() { return products; }
+    
     public void setProducts(List<Product> products) { this.products = products; }
 }
